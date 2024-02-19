c1ba62729fa199e5ef226875a2b2e7d673484880
Joanna Rodriguez <joannarodriguez134@gmail.com>
2024-02-19 02:06:14 -0600

    Initial Commit

---
+++ PlaneDetection/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PlaneDetection
+//
+//  Created by student on 2/19/24.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
